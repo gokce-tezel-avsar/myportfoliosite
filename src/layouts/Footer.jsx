e56c935a561e929ddf3929796721dac72929f639
gokce-tezel-avsar <gokcetezelavsar@gmail.com>
2025-05-03 15:06:46 +0300

    footer tamamlandı

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,20 +1,24 @@
 import React from "react"; 
+
+import { useContext } from "react";
+import { DarkModeContext } from "../context/DarkModeContext";
+import { LanguageContext } from "../context/LanguageContext";
 export default function Footer (){ 
+  const { darkMode } = useContext(DarkModeContext);
+  const { words } = useContext(LanguageContext);
 return (
 <footer className="bg-[#F9f9f9] px-[6rem] py-[4rem] mt-[6rem]">
 <div >
-<h1 tag="h1" className="w-[]">Let's work together on your next products.</h1>
+<h1 tag="h1" className="w-[28rem] text-[2.5rem] leading-[3rem]">Let's work together on your next products.</h1>
 </div>
-<nav className="flex flex-row justify-between mr-[1rem]">
-    <a className=""  href="mailto:gokcetezelavsar@gmail.com">gokcetezelavsar@gmail.com</a>
+<nav className="flex flex-row justify-between mr-[1rem] mt-[4rem]">
+    <a className="text-[#b03048]" href="mailto:gokcetezelavsar@gmail.com">{words.mail}</a>
     <div className="flex flex-row">
-    <a className="no-underline mr-[1rem]" href="https://myportfoliosite-j08nws1af-gokce-tezel-avsars-projects.vercel.app/">Personal Blog</a>
-    <a className="no-underline mr-[1rem] text-[#56ac6c]" href="https://github.com/gokce-tezel-avsar">Github</a>
-    <a className="no-underline text-[#2977b5]" href="https://linkedin.com/in/gökçe-tezel-avşar-25756234a">LinkedIn</a>
+    <a className="no-underline mr-[1rem]"href="https://myportfoliosite-j08nws1af-gokce-tezel-avsars-projects.vercel.app/">{words.siteName}</a>
+    <a className="no-underline mr-[1rem] text-[#56ac6c]"href="https://github.com/gokce-tezel-avsar">{words.socialGit}</a>
+    <a className="no-underline text-[#2977b5]"href="https://aedin.com/in/gökçe-tezel-avşar-25756234a">{words.socialLink}</a>
     </div>
     </nav>
  </footer>
 
 )}
-
-/* background-color:#BAB2E7  : #141414* */
